1ef7aab50eb4897a055ad5299a346634990f5bcf
bsrat mulugeta <bsratmulugetasergawie@gmail.com>
2025-02-07 11:21:00 +0300

    minor improvements and fixes

--- notifications/views.py
+++ notifications/views.py
@@ -1,13 +1,27 @@
-from rest_framework.viewsets import ModelViewSet
+# notifications/views.py
+from rest_framework import viewsets, permissions
+from rest_framework.response import Response
+from rest_framework.decorators import action
 from .models import Notification
 from .serializers import NotificationSerializer
-from rest_framework.permissions import IsAuthenticated
 
-class NotificationViewSet(ModelViewSet):
+class NotificationViewSet(viewsets.ModelViewSet):
     queryset = Notification.objects.all()
     serializer_class = NotificationSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
-        # Filter notifications for the logged-in user
-        return self.queryset.filter(recipient=self.request.user).order_by("-created_at")
+        # Only fetch notifications for the logged-in user
+        return self.queryset.filter(recipient=self.request.user)
+
+    @action(detail=True, methods=['patch'], permission_classes=[permissions.IsAuthenticated])
+    def mark_as_read(self, request, pk=None):
+        # Mark a notification as read
+        notification = self.get_object()
+        notification.read = True
+        notification.save()
+        return Response({"message": "Notification marked as read."})
+     
+
+    # def get_queryset(self):
+    #     return Notification.objects.filter(recipient=self.request.user).order_by('-created_at')
