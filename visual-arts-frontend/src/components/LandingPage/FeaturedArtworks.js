f7fe7eabc5aa88108a70e553eec4911af6697594
bsrat mulugeta <bsratmulugetasergawie@gmail.com>
2025-02-27 17:03:41 +0300

    improved featured artworks

--- visual-arts-frontend/src/components/LandingPage/FeaturedArtworks.js
+++ visual-arts-frontend/src/components/LandingPage/FeaturedArtworks.js
@@ -9,6 +9,7 @@ import "swiper/css/effect-coverflow";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import { EffectCoverflow, Navigation, Pagination } from "swiper/modules";
+import { LeftOutlined, RightOutlined } from "@ant-design/icons"; // Import icons
 
 const { Title, Text } = Typography;
 
@@ -25,66 +26,71 @@ const FeaturedArtworks = () => {
 
     return (
         <div className="p-6 max-w-6xl mx-auto relative">
-            <Title level={3} className="mb-4">Featured Artworks</Title>
+            {/* Title */}
+            <Title level={3} className="mb-6 text-center">Featured Artworks</Title>
+
             {featuredArtworks.length > 0 ? (
                 <div className="relative flex items-center justify-center">
-                    {/* Outer container for keeping arrows separate */}
-                    <div className="relative w-full">
-                        {/* Inner container for artworks */}
-                        <div className="relative mx-auto max-w-4xl">
-                            <Swiper
-                                modules={[EffectCoverflow, Navigation, Pagination]}
-                                effect="coverflow"
-                                grabCursor={true}
-                                centeredSlides={true}
-                                slidesPerView={3}
-                                initialSlide={3}
-                                spaceBetween={5}
-                                coverflowEffect={{
-                                    rotate: 0,
-                                    stretch: 0,
-                                    depth: 200,
-                                    modifier: 2.5,
-                                    slideShadows: false,
-                                }}
-                                navigation={{
-                                    prevEl: ".custom-prev",
-                                    nextEl: ".custom-next",
-                                }}
-                                pagination={{ clickable: true }}
-                                breakpoints={{
-                                    320: { slidesPerView: 1, spaceBetween: 5 },
-                                    768: { slidesPerView: 2, spaceBetween: 10 },
-                                    1024: { slidesPerView: 3, spaceBetween: 10 },
-                                }}
-                            >
-                                {featuredArtworks.map((art) => (
-                                    <SwiperSlide key={art.id} className="swiper-slide-custom">
-                                        <div className="relative">
-                                            <Link to={`/artwork/${art.id}`}>
-                                                <Image
-                                                    src={art.image}
-                                                    alt={art.title}
-                                                    className="rounded-lg object-cover"
-                                                    style={{ height: "300px", width: "100%" }}
-                                                    preview={false}
-                                                />
-                                            </Link>
-                                            {/* Artist Avatar and Name */}
-                                            <div className="absolute bottom-0 left-0 bg-black bg-opacity-50 p-2 flex items-center w-fit rounded-bl-lg rounded-tr-lg">
-                                                <Avatar src={art.artist_avatar} size="small" className="mr-2" />
-                                                <Text className="text-white">{art.artist_name}</Text>
-                                            </div>
+                    {/* Arrows (Positioned Relative to the Artworks) */}
+                    <button className="custom-prev">
+                        <LeftOutlined />
+                    </button>
+                    
+                    {/* Artwork Swiper Container */}
+                    <div className="relative mx-auto max-w-4xl">
+                        <Swiper
+                            modules={[EffectCoverflow, Navigation, Pagination]}
+                            effect="coverflow"
+                            grabCursor={true}
+                            centeredSlides={true}
+                            slidesPerView={3}
+                            initialSlide={5}
+                            spaceBetween={5}
+                            coverflowEffect={{
+                                rotate: 0,
+                                stretch: 0,
+                                depth: 200,
+                                modifier: 2.5,
+                                slideShadows: false,
+                            }}
+                            navigation={{
+                                prevEl: ".custom-prev",
+                                nextEl: ".custom-next",
+                            }}
+                            pagination={{ clickable: true }}
+                            breakpoints={{
+                                320: { slidesPerView: 1, spaceBetween: 5 },
+                                768: { slidesPerView: 2, spaceBetween: 10 },
+                                1024: { slidesPerView: 3, spaceBetween: 10 },
+                            }}
+                        >
+                            {featuredArtworks.map((art) => (
+                                <SwiperSlide key={art.id} className="swiper-slide-custom">
+                                    <div className="relative">
+                                        <Link to={`/artwork/${art.id}`}>
+                                            <Image
+                                                src={art.image}
+                                                alt={art.title}
+                                                className="rounded-lg object-cover"
+                                                style={{ height: "300px", width: "100%" }}
+                                                preview={false}
+                                            />
+                                        </Link>
+                                        {/* Artist Avatar and Name */}
+                                        <div className="absolute bottom-0 left-0 bg-black bg-opacity-50 p-2 flex items-center w-fit rounded-bl-lg rounded-tr-lg">
+                                            <Avatar src={art.artist_avatar} size="small" className="mr-2" />
+                                            <Text className="text-white">{art.artist_name}</Text>
                                         </div>
-                                    </SwiperSlide>
-                                ))}
-                            </Swiper>
-                        </div>
-
-                        {/* Custom navigation arrows, now fully outside artworks */}
-                        <div className="custom-prev"></div>
-                        <div className="custom-next"></div>
+                                    </div>
+                                </SwiperSlide>
+                            ))}
+                        </Swiper>
                     </div>
+
+                    {/* Right Arrow */}
+                    <button className="custom-next">
+                        <RightOutlined />
+                    </button>
                 </div>
             ) : (
                 <Empty description="No featured artworks available" />
