9f0440f54df53e85af0da68748e99146a56c232f
bsrat mulugeta <bsratmulugetasergawie@gmail.com>
2025-03-04 21:07:57 +0300

    fixed displaying likes for randed artists

--- artwork/serializers.py
+++ artwork/serializers.py
@@ -5,10 +5,12 @@ from users.models import CustomUser
 class ArtworkSerializer(serializers.ModelSerializer):
     image = serializers.ImageField(use_url=True) 
     artist_name = serializers.SerializerMethodField()
+    likes_count = serializers.SerializerMethodField()  # Add likes count
+
     
     class Meta:
         model = Artwork
-        fields = ['id', 'title', 'description', 'image', 'artist', 'artist_name', 'feedback', 'approval_status', 'submission_date', 'category']  # ✅ Include 'id' and 'approval_status'
+        fields = ['id', 'title', 'description', 'image', 'artist', 'artist_name', 'feedback', 'approval_status', 'submission_date', 'category', "likes_count"]  # ✅ Include 'id' and 'approval_status'
         read_only_fields = ['approval_status', 'feedback', 'artist', 'submission_date']
         
         
@@ -21,3 +23,6 @@ class ArtworkSerializer(serializers.ModelSerializer):
         request = self.context.get('request')  # Get the request from the context
         validated_data['artist'] = request.user  # Assign the logged-in user
         return super().create(validated_data)
+
+    def get_likes_count(self, obj):
+        return obj.likes.count()  # Return the number of likes
